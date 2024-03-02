caa673622c29d1aae34ce4d1ce474fb7074eda42
ShanR6 <shanzasho123@gmail.com>
2024-03-02 23:54:35 +0200

    [task 1] Add variable reference and concatenation

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -9,3 +9,6 @@ $very_bad_unclear_name = "15 chicken wings";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+$order = &$very_bad_unclear_name;
+$order .= " + 1 cola bottle";
