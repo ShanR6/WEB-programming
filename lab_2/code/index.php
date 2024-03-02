1dd66266cf829b337fa416ae26a66a86f30b4f2b
ShanR6 <shanzasho123@gmail.com>
2024-03-03 00:09:50 +0200

    [task 2] Declaration and output variables

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -13,3 +13,12 @@ echo "\nYour order is: $very_bad_unclear_name.";
 $order = &$very_bad_unclear_name;
 $order .= " + 1 cola bottle";
 echo $very_bad_unclear_name;
+
+// task 2
+echo "<br>"; // "\n" not working properly in browser
+$num1 = 10;
+echo $num1;
+
+echo "<br>";
+$num2 = 3.5;
+echo $num2;
