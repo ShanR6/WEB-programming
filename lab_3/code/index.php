10e56661894bc58833c9fbcbbc34433591a33f94
ShanR6 <shanzasho123@gmail.com>
2024-03-29 14:15:41 +0200

    [task 3] Add initial code

--- lab_3/code/index.php
+++ lab_3/code/index.php
@@ -1,65 +1,13 @@
-<?php
-// task 2c
-session_start();
-
-if ('POST' === $_SERVER['REQUEST_METHOD']) {
-    $name = '';
-    $age = 0;
-    $salary = '';
-    $hobby = '';
-
-    if (isset($_POST['name'])) {
-        $name = $_POST['name'];
-    }
-    if (isset($_POST['age'])) {
-        $age = $_POST['age'];
-    }
-    if (isset($_POST['salary'])) {
-        $salary = $_POST['salary'];
-    }
-    if (isset($_POST['hobby'])) {
-        $hobby = $_POST['hobby'];
-    }
-
-    $infoData = [
-        'name' => $name,
-        'age' => $age,
-        'salary' => $salary,
-        'hobby' => $hobby
-    ];
-
-    $_SESSION['userData'] = $infoData;
-}
-
-if (isset($_SESSION['userData'])) {
-    echo '<ul>';
-    foreach ($_SESSION['userData'] as $key => $value) {
-        echo "<li>$key: $value</li>";
-    }
-    echo '</ul>';
-    echo '<br />';
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-<form method="post">
-    Name:
-    <input type="text" name="name"><br>
 
-    Age:
-    <input type="number" name="age"><br>
-
-    Salary:
-    <input type="text" name="salary"><br>
-    
-    Hobby:
-    <input type="text" name="hobby"><br>
-    <input type="submit" value="Submit">
-</form>
 </body>
 </html>
