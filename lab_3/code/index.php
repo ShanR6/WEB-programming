ee589a4a061a9e9be02de882543310fdf512baf0
ShanR6 <shanzasho123@gmail.com>
2024-03-29 15:12:29 +0200

    [task 3] Steps 1 and 2

--- lab_3/code/index.php
+++ lab_3/code/index.php
@@ -5,9 +5,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Carvago</title>
 </head>
 <body>
+    <form action="save.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
+        <label for="category">Category</label>
+        <select name="category" required>
+            <?php
+            $categories = array_filter(glob("./categories/*"), 'is_dir');
+            foreach ($categories as $category) {
+                $name = basename($category);
+                echo "<option value=\"$name\">$name</option>";
+            }
+            ?>
+        </select>
 
+        <label for="title">Title</label>
+        <input type="text" name="title" required>
+
+        <label for="description">Description</label>
+        <textarea rows="3" name="description"></textarea>
+
+        <input type="submit" value="Save">
+    </form>
 </body>
 </html>
